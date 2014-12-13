f3b3ef4a9b2a4f836ea95b8cd54935898df6e943
Dr. Pete Markiewicz <pindiespace@gmail.com>
2014-12-13 08:00:56 -0800

    prefixfree added

--- public/js/elefart.js
+++ public/js/elefart.js
@@ -5,7 +5,34 @@
 window.elefart = (function () {
 
 	var screens = []
+	TRUE = "true",
+	FALSE = "false",
+	UNKNOWN = -1,
 	firstTime = true;
+
+	/** 
+	 * @method isStandalone
+	 * @param {Enum} os
+	 * @return {Boolean|UNKNOWN} if true or false, return, else return UNKNOWN (-1)
+	 */
+	function isStandalone (type) {
+		if(window.navigator.standalone === undefined) {
+			return true; //we don't want desktop bringing up an install screen!
+		}
+		else {
+			if(type) {
+				switch(type) {
+					case 'ios':
+						return !!(window.navigator.standalone);
+						break;
+					default:
+						break;
+				}
+			}
+
+		}
+		return UNKNOWN;
+	}
 
 	/** 
 	 * @method init
