ee56399f8eb1c554df4068d9040f3ab4e3d278d7
pindiespace <pindiespace@gmail.com>
2014-12-15 17:35:07 -0800

    DOMRect and DOMPoint

--- public/js/elefart-make.js
+++ public/js/elefart-make.js
@@ -23,66 +23,118 @@ window.elefart.make = (function () {
 
 	/* 
 	 * ============================
+	 * UTILITY FUNCTIONS FOR HANDLING SCREEN OBJECTS
+	 * ============================
+	 */
+
+
+	/** 
+	 * @method pointInRect
+	 * determine if a point is inside or outside rect
+	 */
+	function pointInRect (pt, rect) {
+
+	}
+	 
+
+	/** 
+	 * @method rectInRect
+	 * determine if rect1 is completely inside rect2
+	 */
+	function rectInRect (rect1, rect2) {
+
+	}
+
+
+
+
+	/* 
+	 * ============================
 	 * BASIC SCREEN OBJECT FACTORY FUNCTIONS
 	 * ============================
 	 */
 
 	/** 
-	 * @method createPoint
-	 * make a point object
+	 * Objects are defined using the new Geometry interface
+	 * for conversion with CSS transform utilities
+	 * @link https://hacks-dev.allizom.org/2014/04/coordinate-conversion-made-easy/
+	 */
+
+	/** 
+	 * @method DOMPoint
+	 * make a point object following DOMPoint specification
+	 * @link http://www.w3.org/TR/2014/WD-geometry-1-20140522/#DOMPoint
 	 * @param {Number} x the left coordinate on screen
 	 * @param {Number} y the top coordinate on screen
 	 * @returns {Point} returns object with top and left defined
 	 */
-	function createPoint (x, y) {
+	function DOMPoint (x, y, z, w) {
 		return {
-			top:0,
-			left:0
+			x:x,
+			y:y,
+			z:z,
+			w:w
 		};
 	}
 
-	function createSize(width, height) {
+	/** 
+	 * @method DOMRect
+	 * make a point object following DOMRect specification
+	 * @link http://www.w3.org/TR/2014/WD-geometry-1-20140522/#DOMRect
+	 * @param {Number} x the left coordinate on screen
+	 * @param {Number} y the top coordinate on screen
+	 * @returns {Point} returns object with top and left defined
+	 */
+	function DOMRect (point, width, height) {
 		return {
+			x:point.x,
+			y:point.y,
+			top: point.y,
+			left:point.x,
+			bottom:(point.y + height),
+			right:(point.x + width),
 			width:width,
-			height:height
+			height:height,
 		};
 	}
 
-	function createRect (point, size) {
+	/** 
+	 * @method DOMQuad
+	 * @link http://dev.w3.org/fxtf/geometry/#DOMQuad
+	 */
+	function DOMQuad (rect) {
 		return {
-			top: point.top,
-			left:point.left,
-			bottom:(point.top + size.height),
-			right:(point.left + size.width),
-			getWidth: function () {return this.right - this.left;},
-			getHeight: function () {return this.bottom - this.top;}
-		};
+			pt1:new DOMPoint(x, y, 0, 1),
+			pt2:new DOMPint(x+rect.width, y, 0, 1),
+			pt3:new DOMPoint(x+rect.width, y+rect.height, 0, 1),
+			pt4:new DOMPoint(x, y+rect.height, 0, 1)
+		}
 	}
-
 
 	/** 
 	 * @method createScreenBox
-	 * create a shape with padding. Model is a simplified form of the CSS 'box model' but used for canvas
+	 * create a shape with padding. Model is a simplified form of the CSS 'box model' 
+	 * but used for canvas (two overlapping DOMRect objects)
 	 * - define parent
 	 * - read parent padding
 	 * - position based on padding
-	 * @param {Rect} boxRect
-	 * @param {Rect} paddingRect
-	 * @param {Number} layer integer z-position for stacking objects
+	 * @param {Rect} boxRect DOMRect
+	 * @param {Rect} paddingRect DOMRect
+	 * @param {Number} layer integer z-position for stacking objects in display list
 	 */
 	function createScreenBox (parent, boxRect, paddingRect, layer) {
-		if(paddingRect.width > boxRect.getWidth()) {
-			elefart.showError();
-		}
-		if(paddingRect.height > boxRect.getHeight()) {
-
+		if(paddingRect.width > boxRect.width || 
+			paddingRect.height > boxRect.height) {
+			elefart.showError("invalid screenBox dimensions");
+			return;
 		}
 		return {
 			parent:parent,
 			children: [],
 			box:boxRect,
 			padding:paddingRect,
-			innerBox: createRect ()
+			innerBox:boxRect,
+			layer:layer
 		};
 	}
 
@@ -120,9 +172,8 @@ window.elefart.make = (function () {
 	return {
 		init:init,
 		run:run,
-		createPoint:createPoint,
-		createSize:createSize,
-		createRect:createRect
+		createPoint:DOMPoint,
+		createRect:DOMRect
 	};
 
 
