8a2c6e168db40bac26aff8200a320ffb2f1c511b
pindiespace <pindiespace@gmail.com>
2014-12-22 06:42:54 -0800

    more constructors

--- public/js/elefart-building.js
+++ public/js/elefart-building.js
@@ -17,13 +17,7 @@ window.elefart.building = (function () {
 	dashboard,
 	display,
 	controller, 
-	parts = {
-		elevators:[],
-		shafts:[],
-		floors:[],
-		goodies:[],
-		people:[]
-	},
+	world, //the top-level object 
 	firstTime = true;
 
 	/* 
@@ -37,12 +31,13 @@ window.elefart.building = (function () {
 	 * @classdesc an Elevator consists of a single rounded Rect
 	 * representing the elevator, plush some line arcs on top forming 
 	 * the attachment in the shaft
-	 * - parent: elevator shaft
-	 * - grandparent: building
-	 * - chidren: elevator doors, people
+	 * - parent: ElevatorShaft
+	 * - grandparent: Building
+	 * - chidren: ElevatorDoors, Person(s)
 	 */
 	function Elevator () {
 		var elev = {
+			queue: [],
 			disp:factory.ScreenRect(
 				10, 10, 
 				100, 100, 
@@ -52,10 +47,15 @@ window.elefart.building = (function () {
 				display.LAYERS.ELEBACK
 			)
 		};
-		parts.elevators.push(elev);
+
+		//visual features
 		elev.disp.setStroke(4, 'rgb(25,50,100');
 		elev.disp.setFill('rgb(200,100,100');
-			return elev;
+		elev.disp.setLayer(display.LAYERS.ELEBACK);
+
+		//add to elefart.screens display list
+		
+		return elev;
 	}
 
 	/* 
@@ -67,7 +67,11 @@ window.elefart.building = (function () {
 	/** 
 	 * @constructor ElevatorDoors
 	 * @classdesc elevator doors consist of an enclosing rect, with 
-	 * two smaller Rects functioning as the sliding doors
+	 * two smaller Rects functioning as the sliding doors. They are 
+	 * part of the Building, rather than the elevator
+	 * - parent: BuildingFloor
+	 * - grandparent: Building
+	 * - children: none
 	 */
 	function ElevatorDoors () {
 
@@ -83,6 +87,9 @@ window.elefart.building = (function () {
  	 * @constructor ElevatorShaft
  	 * @classdesc the elevator is a long Rect column running vertically 
  	 * in the building.
+ 	 * - parent: Building
+ 	 * - grandparent: World
+ 	 * - children: Elevator(s)
  	 */
  	function ElevatorShaft () {
 
@@ -94,6 +101,15 @@ window.elefart.building = (function () {
 	 * ============================
 	 */
 
+	/** 
+	 * @constructor BuildingFloor
+	 * @classdesc the floors passed by the Elevator. Contains Goodies (until they are 
+	 * picked up by the Person) and any controls that indicate Elevator passage (e.g. 
+	 * a ring when an Elevator arrives at a floor)
+	 * - parent: Building
+	 * - grandparent: World
+	 * - children: ElevatorDoors, Goodies
+	 */
 	function BuildingFloor () {
 
 	}
@@ -104,17 +120,47 @@ window.elefart.building = (function () {
 	 * ============================
 	 */
 
+	/** 
+	 * @constructor Building
+	 * @classdesc the building where the action happens
+	 * - parent: World
+	 * - grandparent: none
+	 * - children: BuildingFloor(s)
+	 */
 	function Building () {
 
 	}
 
-	/* 
-	 * ============================
-	 * PEOPLE
-	 * ============================
+	/** 
+	 * @constructor Sun
+	 * @classdesc the sun over the Building
+	 * - parent: World
+	 * - grandparent: none
+	 * - children: none
 	 */
+	function Sun () {
 
-	function Person () {
+	}
+
+	/** 
+	 * @constructor Sky
+	 * @classdesc the sky over the Building
+	 * - parent: World
+	 * - grandparent: none
+	 * - children: none
+	 */
+	function Sky () {
+
+	}
+
+	/** 
+	 * @constructor World
+	 * @classdesc the top-level environment
+	 * - parent: none
+	 * - grandparent: none
+	 * - children: Building
+	 */
+	function World () {
 
 	}
 
@@ -124,6 +170,13 @@ window.elefart.building = (function () {
 	 * ============================
 	 */
 
+	/** 
+	 * @constructor Gas
+	 * @classdesc the gas used as a weapon in the game
+	 * - parent: Person(s) or Elevators (can transfer)
+	 * - grandparent: ElevatorFloor or Building
+	 * - children: none
+	 */
 	function Gas () {
 
 	}
@@ -134,7 +187,46 @@ window.elefart.building = (function () {
 	 * ============================
 	 */
 
+	/** 
+	 * @constructor Goodie
+	 * @classdesc an item which helps a Person withstand a Gas attack
+	 * - parent: BuildingFloor or Person
+	 * - grandparent: Building or Elevator
+	 * - children: none
+	 */
 	function Goodie () {
+
+	}
+
+	/* 
+	 * ============================
+	 * PEOPLE
+	 * ============================
+	 */
+
+	/** 
+	 * @constructor Person
+	 * @classdesc individual who rides Elevators
+	 * - parent: Elevator or BuildingFloor
+	 * - grandparent: ElevatorShaft or Building
+	 * - children: Gas, Goodie(s)
+	 */
+	function Person () {
+
+	}
+
+	/* 
+	 * ============================
+	 * BUILDING ASSEMBLY
+	 * ============================
+	 */
+
+	/** 
+	 * @method buildWorld
+	 * @description build the world structure, adding visible items to the 
+	 * elefart.screens displayList
+	 */
+	function buildWorld () {
 
 	}
 
@@ -174,14 +266,16 @@ window.elefart.building = (function () {
 
 	//returned object
 	return {
-		Elevator:Elevator,
+		Elevator:Elevator, //constructorss
 		ElevatorDoors:ElevatorDoors,
 		ElevatorShaft:ElevatorShaft,
 		BuildingFloor:BuildingFloor,
 		Building:Building,
+		Sun:Sun,
+		Sky:Sky,
 		Goodie:Goodie,
 		Person:Person,
-		parts:parts,
+		world:world, //the hierarchical world object
 		init:init,
 		run:run
 	};
