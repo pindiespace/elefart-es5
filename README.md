5f01fb981eff51eb3866030b55e1eef4fa357469
Dr. Pete Markiewicz <pindiespace@gmail.com>
2016-06-04 14:41:18 -0700

    draft gameplay

--- README.md
+++ README.md
@@ -6,12 +6,32 @@ An elevator chase where blasts with gas keep you from reaching the top.
 Programmed with:
 - HTML5 standards-based markup
 - CSS/CSS3
+- JavaScript ES5 techniques (no OOP framework)
 - JavaScript polyfills to increas compatibility
 - Fluid (rather than responsive) design techniques
 - No frameworks
 
+About 
+=====
+
+This is not a complete game. It was a draft showing how to build a complex game 
+environment without a framework, using vanilla ES5 and without a framework 
+or extensive OOP configuration. 
+
+The complete game is being built in ES6 at in the "elefart-es6" project.
+
 Versions
 =========
 
-  0.1 - Initial boilerplate
-  
+  0.1.0 - Initial boilerplate
+  0.1.1 - Demonstration of Elevator-style gameplay.
+
+How to Play
+===========
+1. Run npm install
+2. You may have to install some programs (e.g. phantomjs, karma, grunt) manually with the -g option.
+3. To test the character movement, click with a mouse on the same floor as a character.
+4. If the character is in front of an elevator, click in the elevator shaft to send him to a different floor.
+5. Pick up health "goodies" by running past them on a building floor.
+6. Arrow keys bend the player over.
+7. Hit the space bar to break wind.
--- server.js
+++ server.js
@@ -1,122 +1,57 @@
-/** 
- * server.js
- * v 1.0
- * basic web and websockets server
- * - expressJS
- * - websockets 
- * 
- * - for Elefart game
- *
- * Use modules specified for 4.x (unlike many tutorials)
- * http://expressjs.com/migrating-4.html
- *
- * 
- */
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var port = 3000;
 
-
-//INCLUDES
-
-var path    = require('path');           //define paths to files and folders
-var express = require('express');        //express server augments basic Node
-//var io      = require('socket.io');      //CAN'T DO IT HERE!!!!
-//var morgan  = require('morgan');         //standard access logs for the server
-var favicon = require('serve-favicon');  //serve out favicon
-
-//JS FILES SHARED BETWEEN CLIENT AND SERVER
-
-var common = require('./public/js/elefart-netmessage.js');
-
-//test common JS file
-console.log('commonJS object test: news:' + common.NEWS);
-
-//SPECIFY PORT NUMBER FOR SERVER
-var portNum = common.portNum;
-
-//CREATE THE APP USING ExpressJS
-
+// Create the Express server.
 var app = express();
 
-//NO VIEWS (not using Jade)
+// favicon /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-//NO SPECIAL PATHS USED
+// make node_modules accessible
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
-//SET THE STATIC DIRECTORY FOR WEB PAGES
+// make public directory accessible
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, '/public')));
-
-//CREATE THE HTTP SERVER
-
-//add our Express app to the default Node HTTP server
-
-var http = require('http').Server(app);
-
-/** 
- * IMPORTANT
- * we have to 'require' and 'listen' in one line for socket.io to work
- * with express. any attempt to split them up may result in errors!
- */
-var io   = require('socket.io').listen(http);
-
-//SOCKET.IO CALLBACKS
-
-io.sockets.on('connection', function (socket) {
-
-	console.log('HEY! - **SOMEBODY JUST CONNECTED TO US**');
-
-	//send them back a 'server ready' message
-
-	//socket.emit(common.NET_MESSAGE.NEWS, { hello: 'server ready' });
-
-	//callbacks for client messages
-
-	socket.on('disconnect', function () {
-		console.log("client disconnected");
-  	});
-
-	socket.on(common.NEWS, function(data) {
-		console.log("server recieved NEWS: " + data);
-	});
-
-	//send a message to everyone BUT the original sender
-	//@link http://psitsmike.com/2011/10/node-js-and-socket-io-multiroom-chat-tutorial/
-
-	//users
-	socket.on(common.USER_MESSAGE.USER_JOINED, function(data) {
-		console.log("server recieved USER_JOINED: " + data.uName);
-		//send everyone else the connect object from a new user
-		socket.broadcast.emit(common.USER_MESSAGE.USER_JOINED, data);
-	});
-
-	socket.on(common.USER_MESSAGE.USER_MOVED, function(data) {
-		console.log("server recieved USER_MOVED: " + data.uName);
-		//send everyone else the connect object from a new user
-		socket.broadcast.emit(common.USER_MESSAGE.USER_MOVED, data);
-	});
-
-	socket.on(common.USER_MESSAGE.USER_LEFT, function(data) {
-		console.log("server received USER_LEFT: " + data.uName);
-		socket.broadcast.emit(common.USER_MESSAGE.PLAYER_LEFT, data);
-	});	
-
-	//elevators
-	socket.on(common.ELEVATOR_MESSAGE.ELEVATOR_MOVED, function(data) {
-		console.log("server recieved ELEVATOR_MOVED: " + data.elev.shaft);
-		socket.broadcast.emit(common.ELEVATOR_MESSAGE.PLAYER_MOVED, data);		
-	});
-
-
-}); //end of io.sockets.on('connect')
-
-/* FIRE UP THE SERVER */
-
-//http is the server 'decorated' earlier by require('http').server(app)
-
-var server = http.listen(portNum, function (err) {
-	if(err) {
-		console.log('failed to load http server');
-	}
-	else {
-		console.log('Elefart http and websocke (socketio) server listening on port:' + portNum);
-		console.log('CTL-C to shut down server');
-	}
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+if(!port) {
+  port = process.env.PORT;
+}
+
+app.listen(port);
+console.log('server running at port:' + port);
+
+module.exports = app;
